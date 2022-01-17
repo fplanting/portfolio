1b2463f116fe9e8db9f6a7921d043f2b3855facd
fplanting <frida.planting@hotmail.com>
2022-01-17 12:00:32 +0100

    fixed styling + githubrepos

--- js/script.js
+++ js/script.js
@@ -8,7 +8,15 @@
    
        let portfolio = document.getElementById("portfolio");
 
+       portfolio.innerHTML= "";
        for (i=0; i<json.length; i++) {
-           portfolio.insertAdjacentHTML("beforeend", "<a href='"+ json[i].html_url +"' target='_blank'><div class='githubrepo'>" + json[i].name + "</div></a>")
-       }
+       //    portfolio.insertAdjacentHTML("beforeend", "<a href='"+ json[i].html_url +"' target='_blank'><div class='githubrepo'>" + json[i].name + "</div></a>")
+
+       let githubApi = document.createElement("div");
+       githubApi.setAttribute("id", "githubApi");
+       githubApi.innerHTML = "<a href='"+ json[i].html_url +"' target='_blank'><div class='githubrepo'>" + json[i].name + "</div></a>";
+
+       portfolio.append(githubApi);
+    }
 });
+
